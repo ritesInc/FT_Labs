9e27c542c6e9498f162f531e4a33ac438bbe0ab2
Ritesh Mishra -X (ritmish2 - WIPRO LIMITED at Cisco) <ritmish2@partnet.cisco.com>
2020-03-29 22:26:17 +0530

    Changing db from sqlite to mysql

--- ft_labs/ft_labs/settings.py
+++ ft_labs/ft_labs/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'ft_labs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'riteshm$ft_labs',
+        'USER': 'riteshm',
+        'PASSWORD': '--12Unowhoim',
+        'HOST': 'riteshm.mysql.pythonanywhere-services.com',
     }
 }
 
